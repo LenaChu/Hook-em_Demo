2a2abc9fe2133252818184956cee1e32eccb3dbe
LenaChu <a87192611@gmail.com>
2019-04-29 14:26:44 -0500

    Put in unity project.

---
+++ VE_FinalProject_Demo/Assets/Billboarding.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//cameraFacingBillboard.cs v02
+//by Neil Carter (NCarter)
+//modified by Juan Castaneda (juanelo)
+//
+//added in-between GRP object to perform rotations on
+//added auto-find main camera
+//added un-initialized state, where script will do nothing
+
+public class Billboarding : MonoBehaviour
+{
+    public Camera m_Camera;
+    public bool amActive = false;
+    public bool autoInit = false;
+    GameObject myContainer;
+
+    void Awake()
+    {
+        if (autoInit == true)
+        {
+            m_Camera = Camera.main;
+            amActive = true;
+        }
+
+        myContainer = new GameObject();
+        myContainer.name = "GRP_" + transform.gameObject.name;
+        myContainer.transform.position = transform.position;
+        transform.parent = myContainer.transform;
+    }
+
+    //Orient the camera after all movement is completed this frame to avoid jittering
+    void LateUpdate()
+    {
+        if (amActive == true)
+        {
+            myContainer.transform.LookAt(myContainer.transform.position + m_Camera.transform.rotation * Vector3.back, m_Camera.transform.rotation * Vector3.up);
+        }
+    }
+}
---
+++ VE_FinalProject_Demo/Assets/Scripts/Patrol.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+
+    private NavMeshAgent agent;
+    public ThirdPersonCharacter character;
+    public Transform[] moveSpots;
+    private int randomSpot;
+
+    private Vector3 lastPos;
+    private GameObject player;
+    private bool bKeepGoing;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player");
+
+        agent.updateRotation = false;
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0, moveSpots.Length);
+        bKeepGoing = true;
+        lastPos = agent.velocity;
+    }
+
+
+    void keepMoving()
+    {
+        agent.SetDestination(moveSpots[randomSpot].position);
+
+        if (waitTime <= 0)
+        {
+            randomSpot = Random.Range(0, moveSpots.Length);
+            waitTime = startWaitTime;
+        }
+        else
+        {
+            waitTime -= Time.deltaTime;
+        }
+
+
+        if (agent.remainingDistance > agent.stoppingDistance)
+        {
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            character.Move(Vector3.zero, false, false);
+        }
+    }
+
+    void Update()
+    {
+        float diff = Vector3.Distance(player.transform.position, agent.transform.position);
+
+        if (diff < 1.8f)
+        {
+            Debug.Log("Update() STOP diff=" + diff);
+            agent.velocity = Vector3.zero;
+        }
+        else
+        {
+            Debug.Log("Update() GO diff=" + diff);
+            agent.velocity = lastPos;
+            keepMoving();
+        }
+    }
+}
